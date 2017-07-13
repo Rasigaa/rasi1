204c82f8980350d66c4932918a12a41cdb1d05f1
Rasigaa <ranirasi45@gmail.com>
2017-07-13 15:41:33 +0530

    Update and rename guvi8.java to guvi10.java

--- guvi10.java
+++ guvi10.java
@@ -1,14 +1,12 @@
 import java.io.*;
 import java.util.*;
-public class guvi8{
+public class guvi10{
 public static void main(String args[]){
 int i=s.nextInt();
 int j,sum=0;
-if(i!=0){
-for(j=i;j>0;j--)
+for(j=i;j>=0;j--)
 {
 sum=sum+j;
 }
 System.out.println(sum);
-}
 }}
