d86c81ac9211c17db546c5c63e66e41f676f2c30
Rasigaa <ranirasi45@gmail.com>
2017-07-13 20:50:49 +0530

    Update and rename guvi9.java to guvi15.java

--- guvi9.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class guvi9{
-public static void main(String args[]){
-Scanner s=new Scanner(System.in);
-String a=s.next();
-System.out.print(a.length());
-}}
