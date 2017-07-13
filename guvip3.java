80bb2c8744a1daf1f7c372408a332b9b20d9827d
Rasigaa <ranirasi45@gmail.com>
2017-07-13 21:23:37 +0530

    Update guvip3.java

--- guvip3.java
+++ guvip3.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class guvip3{
 public static void main(String args[]){
 Scanner s=new Scanner(System.in);
-StringBuffer a=new StringBuffer(s.nextLine());
-  System.out.print(a.reverse());
+String a=s.nextLine();
+  System.out.print(Integer.parseInt(a));
 }
 }
