8e9a5cc17e375ddf1291abc357869a3331e38967
Rasigaa <ranirasi45@gmail.com>
2017-07-13 20:58:05 +0530

    Update and rename guvi15.java to guvi16.java

---
+++ guvi16.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class guvi16{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+  int i,sum=1;
+  for(i=a;i>0;i--)
+  {
+  sum=sum*i;
+  }
+  System.out.print(sum);
+}}
