9f7e4d61370168a68c68ea5c8b6813891698c90f
Rasigaa <ranirasi45@gmail.com>
2017-07-13 14:27:37 +0530

    Update and rename Guvi3.java to Guvi4.java

---
+++ Guvi4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Guvi4{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+String a=s.next();
+if((int)a.charAt(0)<123&&(int)a.charAt(0)>64)
+System.out.print("yes");
+else
+System.out.print("no");
+}}
