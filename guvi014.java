e2bd3433944a169b4a0a73c7f3c5b4bfcbeee473
Rasigaa <ranirasi45@gmail.com>
2017-07-13 21:14:37 +0530

    Update guvi014.java

--- guvi014.java
+++ guvi014.java
@@ -3,22 +3,7 @@ import java.util.*;
 public class guvi014{
 public static void main(String args[]){
 Scanner s=new Scanner(System.in);
-int i=s.nextInt(),k=0,j;
-  for(j=2;j<i;j++)
-  {
-  if(i%j==0)
-  {
-  k++;
-  }
-  }
-  if(k==0)
-  {
-    System.out.print(i+" "+"is a prime number");
-  }
-  else
-  {
-    System.out.print(i+" "+"is not a prime number");
-  k=0;
-}
+StringBuffer a=new StringBuffer(s.nextLine());
+  System.out.print(a.reverse());
 }
 }
