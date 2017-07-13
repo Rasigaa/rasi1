ace4eab2a84762caae290d65a969b43b629ddf71
Rasigaa <ranirasi45@gmail.com>
2017-07-13 16:17:54 +0530

    Update guvi12.java

--- guvi12.java
+++ guvi12.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
 Scanner s=new Scanner(System.in);
 int i,st=s.nextInt(),en=s.nextInt();
 for(i=st+1;i<en;i++)
-{if(i%2==0)
+{if(i%2!=0)
 System.out.print(i+" ");
 }}
 }
