b169ea4e5a3c6797b4406476331bbe50449f36ec
Rasigaa <ranirasi45@gmail.com>
2017-07-13 21:15:18 +0530

    Update and rename guvi014.java to guvip2.java

--- guvip2.java
+++ guvip2.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class guvi014{
+public class guvip2{
 public static void main(String args[]){
 Scanner s=new Scanner(System.in);
 StringBuffer a=new StringBuffer(s.nextLine());
