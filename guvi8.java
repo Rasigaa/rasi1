9438200f6a87dfe66f0e9eb0567faaa9199accf1
Rasigaa <ranirasi45@gmail.com>
2017-07-13 15:28:01 +0530

    Update and rename guvi3 to guvi8.java

---
+++ guvi8.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class guvi8{
+public static void main(String args[]){
+int i=s.nextInt();
+int j,sum=0;
+if(i!=0){
+for(j=i;j>0;j--)
+{
+sum=sum+j;
+}
+System.out.println(sum);
+}
+}}
