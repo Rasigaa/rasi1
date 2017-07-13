aa9208db46ce8d0f52fcbec36980f1c937275e71
Rasigaa <ranirasi45@gmail.com>
2017-07-13 16:15:32 +0530

    Update and rename guvi9.java to guvi12.java

---
+++ guvi12.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class guvi12{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int i,st=s.nextInt(),en=s.nextInt();
+for(i=st+1;i<en;i++)
+{if(i%2==0)
+System.out.print(i+" ");
+}}
