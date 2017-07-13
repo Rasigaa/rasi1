c9834370c4070257c4c2063ef40593d3efab97f9
Rasigaa <ranirasi45@gmail.com>
2017-07-13 14:11:53 +0530

    Update and rename guvi3 to Guvi3.java

---
+++ Guvi3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Guvi3{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a%2==0)
+System.out.print("even");
+else
+System.out.print("odd");
+}}
