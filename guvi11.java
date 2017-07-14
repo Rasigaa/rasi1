5008a7bc04a179d53a150b3f085a6f9a16ea3878
Rasigaa <ranirasi45@gmail.com>
2017-07-14 09:24:43 +0530

    Update guvi11.java

--- guvi11.java
+++ guvi11.java
@@ -3,8 +3,16 @@ import java.util.*;
 public class guvi11{
 public static void main(String args[]){
 Scanner s=new Scanner(System.in);
-int i,st=s.nextInt(),en=s.nextInt();
+int i,j,k=0,p=0,st=s.nextInt(),en=s.nextInt();
 for(i=st+1;i<en;i++)
-{if(i%2!=0)
-System.out.print(i+" ");
+{
+  for(j=2;j>i;j++)
+  {
+    if(i%j==0)
+      k++;
+    else
+      p++;
+  }
+}
+System.out.print(p);
 }}
